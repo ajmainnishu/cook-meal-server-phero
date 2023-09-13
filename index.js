c7c1536de339f7aeb27b63eedf341a9a27fdb487
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-13 23:21:07 +0600

    feat: Chef url added

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const chefs = require('./data/chefs.json')
 const port = 5000
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Cook & Meal Running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Cook & Meal Running ${port}`)
-  })
+  res.send('Cook & Meal Running')
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chefs);
+})
+
+app.listen(port, () => {
+  console.log(`Cook & Meal Running ${port}`)
+})
